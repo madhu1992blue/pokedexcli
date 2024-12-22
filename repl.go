424b376589e0a29f43179c8d1c0a0103db299efd
Madhusudan N <madhu1992blue@outlook.com>
2024-12-22 14:04:58 +0000

    feat: add repl for user interaction

--- repl.go
+++ repl.go
@@ -1,13 +1,28 @@
 package main
 
 import (
+	"fmt"
+	"bufio"
 	"strings"
+	"os"
 )
 
-func cleanInput(text string) []string {
-	words := strings.Fields(text)
-	for i, w := range words {
-		words[i] = strings.ToLower(w)
+func startRepl() {
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+		moreTokens := scanner.Scan()
+		if !moreTokens {
+			break
+		}
+		input := scanner.Text()
+		cleanedWords := cleanInput(input)
+		if len(cleanedWords) == 0 {
+			continue
+		}
+		fmt.Printf("Your command was: %s\n", cleanedWords[0])
 	}
-	return words
-}	
+}
+func cleanInput(text string) []string {
+	return strings.Fields(strings.ToLower(text))
+}
--- repl_test.go
+++ repl_test.go
@@ -3,18 +3,19 @@ package main
 import (
 	"testing"
 )
+
 func TestCleanInput(t *testing.T) {
-	cases := []struct{
-		input string
+	cases := []struct {
+		input    string
 		expected []string
 	}{
 		{
-			input: "  hello world  ",
+			input:    "  hello world  ",
 			expected: []string{"hello", "world"},
 		},
 		{
-			input: "Charmander Bulbasaur PIKACHU",
-			expected : []string{"charmander", "bulbasaur", "pikachu"},
+			input:    "Charmander Bulbasaur PIKACHU",
+			expected: []string{"charmander", "bulbasaur", "pikachu"},
 		},
 	}
 
